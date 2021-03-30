53a37b3f82b07d9d3dcc212fb31c86dd8094d8c0
Angulema <eugenia.labokha@yandex.by>
2021-03-31 00:42:29 +0300

    sprint/ fix: fix fetch for 60 words

--- src/components/gameSprint/functions.js
+++ src/components/gameSprint/functions.js
@@ -1,18 +1,40 @@
+/* eslint-disable no-nested-ternary */
 const URL = 'https://react-learnwords-example.herokuapp.com/words';
+const MIN_PAGE = 0;
+const MAX_PAGE = 29;
 
-const getRandomNumber = (min = 0, max = 29) => Math.floor(Math.random() * (max - min + 1)) + min;
+// eslint-disable-next-line max-len
+const getRandomNumber = (min = MIN_PAGE, max = MAX_PAGE) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const getWords = async (group = 0, page = getRandomNumber()) => {
-  console.log('page: ', page, ' group:', group);
-  const rawResponse = await fetch(`${URL}?page=${page}&group=${group}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  const content = await rawResponse.json();
+  const page1 = page < MAX_PAGE ? page + 1 : page - 1;
+  const page2 = page < (MAX_PAGE - 1) ? page + 2
+    : (page < MAX_PAGE ? page - 1 : page - 2);
 
-  // console.log(content);
+  console.log('page: ', page, 'page1: ', page1, 'page2: ', page2, ' group:', group);
+
+  const urls = [
+    `${URL}?page=${page}&group=${group}`,
+    `${URL}?page=${page1}&group=${group}`,
+    `${URL}?page=${page2}&group=${group}`,
+  ];
+
+  const requests = urls.map((url) => fetch(url));
+
+  const content = Promise.all(requests)
+    .then((responses) => Promise.all(responses.map((resp) => resp.json())))
+    .then((tripleArray) => tripleArray.flat())
+    .catch((error) => console.log(error));
+
+  // const rawResponse = await fetch(`${URL}?page=${page}&group=${group}`, {
+  //   method: 'GET',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  // });
+  // const content = await rawResponse.json();
+
+  console.log(content);
   return content;
 };
 
--- src/components/gameSprint/gameSprint.jsx
+++ src/components/gameSprint/gameSprint.jsx
@@ -1,18 +1,30 @@
 import { useState, useEffect } from 'react';
 import GameTimer from './timer';
-import { getWords, getRandomNumber } from './functions';
+import { getWords } from './functions';
 import './gameSprint.scss';
 
 const GameSprint = () => {
   const [isGameStarted, setGameStarted] = useState(true);
   const [isGameFinished, setGameFinished] = useState(false);
   const [allWords, setAllWords] = useState([]);
+  const [workingWords, setWorkingWords] = useState([]);
   useEffect(() => {
+    // надо как-то тянуть сразу 60 слов?..
     getWords(2)
       .then((data) => setAllWords((words) => [...words, ...data]));
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   console.log(allWords);
+  useEffect(() => {
+    // для создания рабочего массива слов
+    // слово, перевод, верный ли сет
+    allWords.forEach((wordSet) => {
+      const { word } = wordSet;
+      const translation = wordSet.wordTranslate;
+      setWorkingWords((words) => [...words, { word, translation, isTrue: true }]);
+    });
+  }, [allWords]);
+  console.log(workingWords);
 
   return (
     <div>
