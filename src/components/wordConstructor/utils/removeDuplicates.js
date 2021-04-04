51bd6d30fe1018a371d08e43727701a93f942139
Samvel <39568849+ssamvelk@users.noreply.github.com>
2021-04-04 16:22:00 +0400

    wordConstructor / feat: add utils fn removeDuplicates

---
+++ src/components/wordConstructor/utils/removeDuplicates.js
@@ -0,0 +1,18 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
+const removeDuplicates = (originalArray, prop) => {
+  const newArray = [];
+  const lookupObject = {};
+
+  for (const i in originalArray) {
+    lookupObject[originalArray[i][prop]] = originalArray[i];
+  }
+
+  for (const i in lookupObject) {
+    newArray.push(lookupObject[i]);
+  }
+  console.log('lookupObject ', lookupObject);
+  return newArray;
+};
+
+export default removeDuplicates;
