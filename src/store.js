0401b09962c545fa4dca42658bdfaf2ee1547a5e
Yuliya Reutovich <yrevtovich@mail.ru>
2021-04-05 04:39:59 +0300

    audiochellenge/refactor: remove logger

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger, ReduxThunk),
+  applyMiddleware(ReduxThunk),
 );
 
 export default store;
