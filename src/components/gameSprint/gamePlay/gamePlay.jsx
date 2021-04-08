5ef45d067822205faa8283236b1f6a064452fe3b
Angulema <eugenia.labokha@yandex.by>
2021-04-09 00:43:36 +0300

    sprint / feat: partly added key-events

--- src/components/gameSprint/gamePlay/gamePlay.jsx
+++ src/components/gameSprint/gamePlay/gamePlay.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useSelector } from 'react-redux';
+import hotkeys from 'hotkeys-js';
 import GameSoundButton from '../../gameSoundButton/gameSoundButton';
 import CloseIconButton from '../../closeIconButton/closeIconButton';
 import playSound from '../../../utils/playSound';
@@ -59,6 +60,44 @@ const GamePlay = ({
     }
   }
 
+  const rightAnswerHandler = () => {
+    if (isSound) playSound(correct);
+    setScore((points) => points + answerPoints);
+
+    if (stringOfRights === 4 || stringOfRights === 8 || stringOfRights === 12) {
+      setAnswerPoints((points) => points + BASE_POINTS);
+    }
+    setStringOfRights((answer) => answer + 1);
+    if (maxStringOfRights < stringOfRights) setMaxStringOfRights(stringOfRights);
+    setRightAnswers((answers) => [...answers, workingWords[0]]);
+  };
+
+  const wrongAnswerHandler = () => {
+    if (isSound) playSound(error);
+    setStringOfRights(0);
+    setAnswerPoints(BASE_POINTS);
+    setWrongAnswers((answers) => [...answers, workingWords[0]]);
+  };
+
+  hotkeys('left', {
+    keyup: false,
+    keydown: true,
+  }, (event, handler) => {
+    event.preventDefault();
+    rightAnswerHandler();
+    // не работает то, что ниже. и баллы не добавляются
+    setWorkingWords((words) => words.slice(1));
+  });
+  hotkeys('right', {
+    keyup: false,
+    keydown: true,
+  }, (event, handler) => {
+    event.preventDefault();
+    wrongAnswerHandler();
+    // не работает то, что ниже
+    setWorkingWords((words) => words.slice(1));
+  });
+
   useEffect(() => {
     console.log('allWords in the useEffect on gamePlay', allWords);
     // для создания рабочего массива слов. если не перезагружать страницу,
@@ -97,20 +136,9 @@ const GamePlay = ({
     // console.log(value);
     // console.log(workingWords[0].isTrue.toString());
     if (value === workingWords[0].isTrue.toString()) {
-      if (isSound) playSound(correct);
-      setScore((points) => points + answerPoints);
-
-      if (stringOfRights === 4 || stringOfRights === 8 || stringOfRights === 12) {
-        setAnswerPoints((points) => points + BASE_POINTS);
-      }
-      setStringOfRights((answer) => answer + 1);
-      if (maxStringOfRights < stringOfRights) setMaxStringOfRights(stringOfRights);
-      setRightAnswers((answers) => [...answers, workingWords[0]]);
+      rightAnswerHandler();
     } else {
-      if (isSound) playSound(error);
-      setStringOfRights(0);
-      setAnswerPoints(BASE_POINTS);
-      setWrongAnswers((answers) => [...answers, workingWords[0]]);
+      wrongAnswerHandler();
     }
     if (workingWords.length > 1) {
       setWorkingWords((words) => words.slice(1));
@@ -141,7 +169,7 @@ const GamePlay = ({
               </div>
             )}
           </div>
-          <p>
+          <p className="game-play__upper_text">
             {`+${answerPoints} баллов`}
           </p>
         </div>
@@ -171,12 +199,18 @@ const GamePlay = ({
           </div>
         )}
         <div className="game-play__buttons">
-          <button value="true" type="button" className="right" onClick={handleAnswer}>
-            Верно
-          </button>
-          <button value="false" type="button" className="wrong" onClick={handleAnswer}>
-            Неверно
-          </button>
+          <div className="game-play__buttons_block">
+            <button value="true" type="button" className="answer-button right" onClick={handleAnswer}>
+              Верно
+            </button>
+            <i className="fas fa-arrow-left" />
+          </div>
+          <div className="game-play__buttons_block">
+            <button value="false" type="button" className="answer-button wrong" onClick={handleAnswer}>
+              Неверно
+            </button>
+            <i className="fas fa-arrow-right" />
+          </div>
         </div>
       </main>
     </>
