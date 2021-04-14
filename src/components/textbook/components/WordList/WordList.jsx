c716f87ad439de26b19f92a9fbe44068d65b70c5
Samvel <39568849+ssamvelk@users.noreply.github.com>
2021-04-14 17:35:11 +0400

    textbook / feat: add WorldList component

--- src/components/textbook/components/WordList/WordList.jsx
+++ src/components/textbook/components/WordList/WordList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import WordListHeadItem from './WordListHeadItem';
 import WordListItem from './WordListItem';
 import './WordList.scss';
 
 const WordList = ({ words }) => (
   <ul className="WorldList">
+    <WordListHeadItem />
     {words.map((el, index) => (
-      <li className="WorldList__item" key={index}>
+      <li className="WorldList__item" key={index + 1}>
         <WordListItem word={el} />
       </li>
     ))}
