005c731efea2ca8fa88484a02d6282c978b54d6b
snegurova <snegurova@gmail.com>
2021-04-14 23:32:20 +0300

    feat: add data fetching

--- src/components/appStatisticsPage/modules/LineChart.jsx
+++ src/components/appStatisticsPage/modules/LineChart.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
+import Preloader from '../../wordConstructor/Game/components/Preloader';
+import { BACK_URL } from '../../../utils/constants';
+import FetchData from '../../../utils/fetchData';
 import './lineChart.scss';
 
 const dataDaily = {
@@ -41,17 +44,42 @@ const options = {
   },
 };
 
-const LineChart = () => (
-  <div className="line-charts__wrapper">
-    <div className="line-chart__wrapper">
-      <h3>Изучено слов за каждый день</h3>
-      <Line data={dataDaily} options={options} />
+const LineChart = () => {
+  const [learnedWords, setLearnedWords] = useState(0);
+  // async function fetchData() {
+  //   // await setIsLoading(true);
+  //   const data = await fetch(`${BACK_URL}/words?page=
+  //   ${currentPage}&group=${currentGroup}`).then((d) => d.json());
+  //   await setWords(data);
+  //   await setIsLoading(false);
+  // }
+
+  const fetch = new FetchData(BACK_URL);
+
+  useEffect(() => {
+    fetch.getUsersStats('6076ba93bca34e00152daf3d', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNzZiYTkzYmNhMzRlMDAxNTJkYWYzZCIsImlhdCI6MTYxODQyMjM0MSwiZXhwIjoxNjE4NDM2NzQxfQ.J0VNepH0GBVqRIBX4eHWgciecIYS33LO32VgDBLKYCc')
+      .then((data) => {
+        console.log(data, data.learnedWords);
+        setLearnedWords(data.learnedWords);
+      });
+  });
+
+  return (
+    <div className="line-charts__wrapper">
+      <div>
+        learned
+        {learnedWords}
+      </div>
+      <div className="line-chart__wrapper">
+        <h3>Изучено слов за каждый день</h3>
+        <Line data={dataDaily} options={options} />
+      </div>
+      <div className="line-chart__wrapper">
+        <h3>Изучено слов за весь период</h3>
+        <Line data={dataTotal} options={options} />
+      </div>
     </div>
-    <div className="line-chart__wrapper">
-      <h3>Изучено слов за весь период</h3>
-      <Line data={dataTotal} options={options} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default LineChart;
