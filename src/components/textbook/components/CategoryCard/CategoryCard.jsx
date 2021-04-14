a0cef0dc2b1cac1a440ee9cb9141248c1bd96db3
Samvel <39568849+ssamvelk@users.noreply.github.com>
2021-04-14 04:07:39 +0400

    textbook / feat: add CategoryCard component

---
+++ src/components/textbook/components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CategoryCard.scss';
+import difficulty1 from '../../assets/img/difficulty-1.png';
+import difficulty2 from '../../assets/img/difficulty-2.png';
+import difficulty3 from '../../assets/img/difficulty-3.png';
+import difficulty4 from '../../assets/img/difficulty-4.png';
+import difficulty5 from '../../assets/img/difficulty-5.png';
+import difficulty6 from '../../assets/img/difficulty-6.png';
+
+const images = [difficulty1, difficulty2, difficulty3, difficulty4, difficulty5, difficulty6];
+
+const CategoryCard = ({ number }) => (
+  <article className="CategoryCard">
+    <img className="CategoryCard__image" src={images[number - 1]} alt="Category" />
+    <span className="CategoryCard__text">{`Раздел ${number}`}</span>
+  </article>
+);
+
+export default CategoryCard;
