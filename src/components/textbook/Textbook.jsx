e7e582f1050cdcfe85c4f76fe1a9616d81ba36cc
Samvel <39568849+ssamvelk@users.noreply.github.com>
2021-04-14 18:01:32 +0400

    textbook / feat: add WordList on page

--- src/components/textbook/Textbook.jsx
+++ src/components/textbook/Textbook.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import SettingsButton from './components/SettingsButton/SettingsButton';
 import CategoryList from './components/CategoryList/CategoryList';
 import TextbookPage from './components/TextbookPage/TextbookPage';
-import WordCard from '../wordCard/wordCard';
 import VocabluarySettings from '../vocabluarySettings/vocabluarySettings';
 import './Textbook.scss';
 
@@ -11,7 +10,7 @@ const Textbook = (props) => {
   // location, location: { page },
   // console.log('location ', location);
   // console.log('page ', page);
-  console.log('section ', section);
+  // console.log('section ', section);
 
   const [page, setPage] = useState(section);
   const [isSettings, setIsSettings] = useState(false);
@@ -23,24 +22,6 @@ const Textbook = (props) => {
 
   const toggleWordCard = () => {
     setIsWordCardOpen((isOpen) => !isOpen);
-  };
-
-  // mocked data for testing WordCard component
-  const wordData = {
-    id: '5e9f5ee35eb9e72bc21af4ca',
-    group: 0,
-    page: 2,
-    word: 'chart',
-    image: 'files/03_0043.jpg',
-    audio: 'files/03_0043.mp3',
-    audioMeaning: 'files/03_0043_meaning.mp3',
-    audioExample: 'files/03_0043_example.mp3',
-    textMeaning: 'A <i>chart</i> is a list of information.',
-    textExample: 'We used a <b>chart</b> to see how we had improved.',
-    transcription: '[tʃɑːrt]',
-    textExampleTranslate: 'Мы использовали график, чтобы увидеть, как мы улучшили',
-    textMeaningTranslate: 'Диаграмма - это список информации',
-    wordTranslate: 'диаграмма',
   };
 
   useEffect(() => {
@@ -57,18 +38,6 @@ const Textbook = (props) => {
       {isSettings && (
       <VocabluarySettings close={toggleSettings} />
       )}
-
-      {/* {isWordCardOpen && (
-      <WordCard
-        wordData={wordData}
-        showNext={() => {}}
-        showPreviouse={() => {}}
-        deleteWord={() => {}}
-        moveToDifficult={() => {}}
-        close={toggleWordCard}
-      />
-      )} */}
-
     </div>
   );
 };
--- src/components/textbook/components/TextbookPage/TextbookPage.jsx
+++ src/components/textbook/components/TextbookPage/TextbookPage.jsx
@@ -1,11 +1,40 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import Preloader from '../../../wordConstructor/Game/components/Preloader';
 import GamesList from '../GamesList/GamesList';
+import WordList from '../WordList/WordList';
 import SettingsButton from '../SettingsButton/SettingsButton';
+import WordCard from '../../../wordCard/wordCard';
+import { BACK_URL } from '../../../../utils/constants';
 import './TextbookPage.scss';
 
 const TextbookPage = ({ page }) => {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isWordCardOpen, setIsWordCardOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentGroup, setCurrentGroup] = useState(page);
+  const [words, setWords] = useState([]);
+
+  async function fetchData() {
+    const data = await fetch(`${BACK_URL}words?page=${currentPage}&group=${currentGroup}`).then((d) => d.json());
+    await setWords(data);
+    await setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log('words ', words);
+  }, [words]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchData();
+  }, [page]);
 
   if (isLoading) return <Preloader />;
 
@@ -14,6 +43,18 @@ const TextbookPage = ({ page }) => {
       <SettingsButton />
       <h2 className="Textbook__title">{`Раздел ${page}`}</h2>
       <GamesList />
+      <WordList words={words} />
+
+      {/* {isWordCardOpen && (
+      <WordCard
+        wordData={words[0]}
+        showNext={() => {}}
+        showPreviouse={() => {}}
+        deleteWord={() => {}}
+        moveToDifficult={() => {}}
+        close={setIsWordCardOpen(!isWordCardOpen)}
+      />
+      )} */}
     </main>
   );
 };
