9e11bfcda06e673ff6a04ddf9e52311d490372b8
snegurova <snegurova@gmail.com>
2021-04-17 00:15:32 +0300

    fix: get long stats new body - removed id from the response

--- src/utils/longStatBody.js
+++ src/utils/longStatBody.js
@@ -2,25 +2,23 @@ const getNewLongStatBody = (response, learned, dateString) => {
   const learnedWords = response.learnedWords + learned;
   if (dateString in response.optional) {
     const learnedByDate = response.optional[dateString] + learned;
-    const body = {
-      ...response,
+    const newBody = {
       learnedWords,
       optional: {
         ...response.optional,
         [dateString]: learnedByDate,
       },
     };
-    return body;
+    return newBody;
   }
-  const body = {
-    ...response,
+  const newBody = {
     learnedWords,
     optional: {
       ...response.optional,
       [dateString]: learned,
     },
   };
-  return body;
+  return newBody;
 };
 
 export default getNewLongStatBody;
